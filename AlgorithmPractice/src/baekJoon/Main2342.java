a559d0e4453966021b93b50e5cb51c43050de2b6
임채현 <dlacogus5239@naver.com>
2023-03-02 16:46:21 +0900

    [Algo] 백준 2342 Dance Dance Revolution

--- AlgorithmPractice/src/baekJoon/Main2342.java
+++ AlgorithmPractice/src/baekJoon/Main2342.java
@@ -3,7 +3,6 @@ package baekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main2342 {
@@ -15,20 +14,55 @@ public class Main2342 {
 	// 2 : Left
 	// 3 : Down
 	// 4 : Right
-	static int[][] position = { { 1, 1 }, { 1, 0 }, { 0, 1 }, { 1, 2 }, { 2, 1 } };
-	static int[][] dp;
+	static int[][][] dp;
+	// (left, right)
+	static int[] foot = { 0, 0 };
+	static int length;
+
 	public static void main(String[] args) throws IOException, NumberFormatException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int length = st.countTokens();
+		length = st.countTokens();
 		movement = new int[length - 1];
+		dp = new int[length + 1][5][5];
 		for (int i = 0; i < length - 1; i++) {
 			movement[i] = Integer.parseInt(st.nextToken());
 		}
 		br.close();
 		// input END
-		
+		int answer = DDR(0, 0, 0);
+		System.out.println(answer);
+	}
+
+	public static int DDR(int step, int left, int right) {
+		if (step == length - 1) {
+			return 0;
+		}
+		if (dp[step][left][right] != 0) {
+			return dp[step][left][right];
+		}
+
+		int leftCost = Cost(left, movement[step]) + DDR(step + 1, movement[step], right);
+		int rightCost = Cost(right, movement[step]) + DDR(step + 1, left, movement[step]);
+
+		dp[step][left][right] = Math.min(leftCost, rightCost);
+
+		return dp[step][left][right];
+	}
+
+	public static int Cost(int cur, int next) {
+		if (cur == next) {
+			return 1;
+		}
+		if (cur == 0) {
+			return 2;
+		}
+		if (Math.abs(cur - next) == 2) {
+			return 4;
+		}
+		return 3;
+
 	}
 
 }
