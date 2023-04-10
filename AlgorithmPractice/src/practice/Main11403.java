84d3914832719880aa2005374d5c183f3b564d75
임채현 <dlacogus5239@naver.com>
2023-04-10 17:29:12 +0900

    [Algo] 백준 11403 경로 찾기(플로이드와샬 중요함)

---
+++ AlgorithmPractice/src/practice/Main11403.java
@@ -0,0 +1,46 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main11403 {
+	// 백준 11403 경로 찾기
+	static int N;
+	static int[][] map;
+
+	public static void main(String[] args) throws IOException, NumberFormatException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+
+		map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][k] != 0 && map[k][j] != 0) {
+						map[i][j] = 1;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
