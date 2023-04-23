16667a79ca24f510500b35d90b07a6594058ca2a
임채현 <dlacogus5239@naver.com>
2023-04-23 20:54:00 +0900

    [Algo] 백준 11279  최대 힙

---
+++ AlgorithmPractice/src/baekJoon/Main11279.java
@@ -0,0 +1,35 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main11279 {
+	// 백준 11279 최대 힙
+	public static void main(String[] args) throws IOException, NumberFormatException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return -Integer.compare(o1, o2);
+			}
+		});
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int cur = Integer.parseInt(br.readLine());
+			if (cur == 0) {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
+			} else {
+				pq.offer(cur);
+			}
+		}
+
+	}
+
+}
