1e35a71b4f95db7f6d83079f13ecdc918fbe0f98
임채현 <dlacogus5239@naver.com>
2023-05-04 18:51:05 +0900

    [Algo] 백준 9095 1, 2, 3 더하기 dfs로 풀이

--- AlgorithmPractice/src/baekJoon/Main9095.java
+++ AlgorithmPractice/src/baekJoon/Main9095.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 
 public class Main9095 {
 	// 백준 9095 1, 2, 3 더하기
+	// n은 11보다 작아서 dfs로 다 구해놓고 출력만 했다.
+	// 다이나믹 프로그래밍이 맞을거 같은데 그 풀이 도전해보기
 	static int[] dp = new int[12];
 
 	public static void main(String[] args) throws IOException, NumberFormatException {
