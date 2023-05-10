eed6a49e5e01074c92d9674fe4a6390d4106c5c1
임채현 <dlacogus5239@naver.com>
2023-05-10 15:48:17 +0900

    [Algo] 백준 15652 N과M (4) - 중복조합

---
+++ AlgorithmPractice/src/baekJoon/Main15652.java
@@ -0,0 +1,44 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main15652 {
+	// 백준 15652 N과 M (4)
+	static int N, M;
+	static int[] selected;
+
+	public static void main(String[] args) throws IOException, NumberFormatException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		selected = new int[M];
+
+		dupComb(1, 0);
+
+	}
+
+	public static void dupComb(int start, int cnt) {
+		if (cnt == M) {
+			print();
+			return;
+		}
+		for (int i = start; i <= N; i++) {
+			selected[cnt] = i;
+			dupComb(i, cnt + 1);
+		}
+	}
+
+	public static void print() {
+		for (int i = 0; i < selected.length; i++) {
+			System.out.print(selected[i] + " ");
+		}
+
+		System.out.println();
+	}
+
+}
