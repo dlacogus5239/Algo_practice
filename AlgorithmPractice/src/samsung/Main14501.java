5a81c9ce6106790d8ba33582a6b9f97fb523e4fe
임채현 <dlacogus5239@naver.com>
2023-07-06 17:32:25 +0900

    [Algo] 백준 14501 퇴사

--- AlgorithmPractice/src/samsung/Main14501.java
+++ AlgorithmPractice/src/samsung/Main14501.java
@@ -13,6 +13,8 @@ public class Main14501 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
+		T = new int[N + 1];
+		P = new int[N + 1];
 		StringTokenizer st;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -20,12 +22,23 @@ public class Main14501 {
 			P[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int[] dp = new int[N + 1];
+		int[] dp = new int[N + 2];
+		dp[0] = 0;
 		for (int i = 0; i < N; i++) {
 			int day = T[i];
 			int cost = P[i];
-			
+			if (i + day <= N) {
+				dp[i + day] = Math.max(dp[i + day], dp[i] + cost);
+			}
+			dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+
 		}
+		int max = Integer.MIN_VALUE;
+
+		for (int i = 1; i < N + 1; i++) {
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
 	}
 
 }
