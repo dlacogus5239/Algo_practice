e2e0d6edb731c1a2af2f52fa78d36b32757f785e
임채현 <dlacogus5239@naver.com>
2024-04-04 17:07:56 +0900

    [Algo] 코드트리 삼성 문제 정리

---
+++ AlgorithmPractice/src/codeTreeSamsung/Main29.java
@@ -0,0 +1,32 @@
+package codeTreeSamsung;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main29 {
+	static int N;
+	static int result = 0;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		makeBeautifulNumber(0);
+
+		System.out.println(result);
+
+	}
+
+	public static void makeBeautifulNumber(int cnt) {
+		if (cnt == N) {
+			result++;
+			return;
+		} else if (cnt > N) {
+			return;
+		}
+
+		for (int i = 1; i <= 4; i++) {
+			makeBeautifulNumber(cnt + i);
+		}
+	}
+
+}
