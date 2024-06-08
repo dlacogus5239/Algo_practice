cdc38c088e43e4fa49fb88b6cb8922e3683cbf0a
임채현 <dlacogus5239@naver.com>
2024-06-08 15:07:01 +0900

    [Algo] Dynamic Programming 0608

--- AlgorithmPractice/src/dynamicProgramming/Main2193.java
+++ AlgorithmPractice/src/dynamicProgramming/Main2193.java
@@ -1,5 +1,6 @@
 package dynamicProgramming;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main2193 {
@@ -8,21 +9,22 @@ public class Main2193 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
-		int[] one = new int[N + 1];
-		int[] zero = new int[N + 1];
-
+		long[] one = new long[N + 1];
+		long[] zero = new long[N + 1];
+		long[] dp = new long[N + 1];
 		one[1] = 1;
 		zero[1] = 0;
-
+		dp[1] = 1;
 //		one[2] = 0;
 //		zero[2] = 1;
 
 		for (int i = 2; i <= N; i++) {
 			one[i] = zero[i - 1];
 			zero[i] = one[i - 1] + zero[i - 1];
+			dp[i] = one[i] + zero[i];
 		}
-
-		System.out.println(one[N] + zero[N]);
+//		System.out.println(Arrays.toString(dp));
+		System.out.println(dp[N]);
 	}
 
 }
